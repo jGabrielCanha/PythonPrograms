42226e3ca9bbcea2d48c8d5c3ea79e36640e0ad8
jGabrielCanha <111362328+jGabrielCanha@users.noreply.github.com>
2023-09-07 18:03:24 -0300

    Add files via upload
    Desenvolva um programa que leia pelo teclado os valores x, y e z de um vetor de 3 dimensões (ou preencha com valores aleatórios). Em seguida, apresente ao usuário um menu com as seguintes opções:
    
    Calcular o tamanho do vetor;
    Normalizar o vetor, apresentando o vetor resultante da normalização;
    Adicionar outro vetor ao que foi lido anteriormente, lendo os valores x, y e z deste novo vetor;
    Subtrair outro vetor ao que foi lido anteriormente, lendo os valores x, y e z deste novo vetor;
    Ler o valor de um escalar e realizar a multiplicação do mesmo pelo vetor, mostrando o vetor resultante;
    Ler o valor de um escalar e realizar a divisão do mesmo pelo vetor, mostrando o vetor resultante;
    Calcular o produto escalar do vetor lido anteriormente por outro vetor, lendo os valores x, y e z deste novo vetor e mostrando o resultado na tela.
    Desenvolva um programa que leia pelo teclado os valores de uma matriz 3x3 (ou preencha com valores aleatórios), e então apresente um menu com as seguintes opções:
    
    Adição e subtração de outra matriz, lendo os valores dessa outra matriz 3x3 e mostrando o resultado na tela;
    Multiplicação e Divisão de um escalar lido pelo teclado;
    Multiplicação da matriz por um vetor de 3 elementos, este lido pelo teclado;
    Multiplicação da matriz por outra matriz 3x3, esta lida pelo teclado;
    Apresentar a transposta da matriz lida inicialmente.

---
+++ VetMatrizes1.py
@@ -0,0 +1,80 @@
+import math
+
+def read_vector():
+    x = float(input("Digite o valor de x: "))
+    y = float(input("Digite o valor de y: "))
+    z = float(input("Digite o valor de z: "))
+    return [x, y, z]
+
+def calculate_vector_length(vector):
+    return math.sqrt(sum(x ** 2 for x in vector))
+
+def normalize_vector(vector):
+    length = calculate_vector_length(vector)
+    return [x / length for x in vector]
+
+def add_vectors(vector1, vector2):
+    return [x + y for x, y in zip(vector1, vector2)]
+
+def subtract_vectors(vector1, vector2):
+    return [x - y for x, y in zip(vector1, vector2)]
+
+def multiply_vector_by_scalar(vector, scalar):
+    return [x * scalar for x in vector]
+
+def divide_vector_by_scalar(vector, scalar):
+    return [x / scalar for x in vector]
+
+def calculate_dot_product(vector1, vector2):
+    return sum(x * y for x, y in zip(vector1, vector2))
+
+def main():
+    x, y, z = read_vector()
+
+    while True:
+        print("\nMenu:")
+        print("1. Calcular o tamanho do vetor")
+        print("2. Normalizar o vetor")
+        print("3. Adicionar outro vetor")
+        print("4. Subtrair outro vetor")
+        print("5. Multiplicar vetor por escalar")
+        print("6. Dividir vetor por escalar")
+        print("7. Calcular produto escalar")
+        print("8. Sair")
+
+        choice = int(input("Escolha uma opção: "))
+
+        if choice == 1:
+            length = calculate_vector_length([x, y, z])
+            print("O tamanho do vetor é:", length)
+        elif choice == 2:
+            normalized_vector = normalize_vector([x, y, z])
+            print("Vetor normalizado:", normalized_vector)
+        elif choice == 3:
+            vector2 = read_vector()
+            result = add_vectors([x, y, z], vector2)
+            print("Resultado da adição:", result)
+        elif choice == 4:
+            vector2 = read_vector()
+            result = subtract_vectors([x, y, z], vector2)
+            print("Resultado da subtração:", result)
+        elif choice == 5:
+            scalar = float(input("Digite o valor do escalar: "))
+            result = multiply_vector_by_scalar([x, y, z], scalar)
+            print("Resultado da multiplicação:", result)
+        elif choice == 6:
+            scalar = float(input("Digite o valor do escalar: "))
+            result = divide_vector_by_scalar([x, y, z], scalar)
+            print("Resultado da divisão:", result)
+        elif choice == 7:
+            vector2 = read_vector()
+            result = calculate_dot_product([x, y, z], vector2)
+            print("Produto escalar:", result)
+        elif choice == 8:
+            print("Saindo do programa.")
+            break
+        else:
+            print("Opção inválida. Escolha novamente.")
+
+if __name__ == "__main__":
+    main()
---
+++ VetMatrizes2.py
@@ -0,0 +1,75 @@
+def read_matrix(rows, cols):
+    matrix = []
+    for i in range(rows):
+        row = []
+        for j in range(cols):
+            value = float(input(f"Digite o valor para a posição ({i+1}, {j+1}): "))
+            row.append(value)
+        matrix.append(row)
+    return matrix
+
+def add_matrices(matrix1, matrix2):
+    return [[x + y for x, y in zip(row1, row2)] for row1, row2 in zip(matrix1, matrix2)]
+
+def subtract_matrices(matrix1, matrix2):
+    return [[x - y for x, y in zip(row1, row2)] for row1, row2 in zip(matrix1, matrix2)]
+
+def multiply_matrix_by_scalar(matrix, scalar):
+    return [[x * scalar for x in row] for row in matrix]
+
+def multiply_matrix_by_vector(matrix, vector):
+    return [sum(matrix[i][j] * vector[j] for j in range(len(vector))) for i in range(len(matrix))]
+
+def multiply_matrices(matrix1, matrix2):
+    return [[sum(matrix1[i][k] * matrix2[k][j] for k in range(len(matrix2[0]))) for j in range(len(matrix2[0]))] for i in range(len(matrix1))]
+
+def transpose_matrix(matrix):
+    return [[matrix[j][i] for j in range(len(matrix))] for i in range(len(matrix[0]))]
+
+def print_matrix(matrix):
+    for row in matrix:
+        print(row)
+
+def main():
+    rows, cols = 3, 3
+    matrix = []
+
+    while True:
+        print("\nMenu:\n1. Adição\n2. Subtração\n3. Multiplicação escalar\n4. Multiplicação vetor\n5. Multiplicação matriz\n6. Transposta\n7. Sair")
+        choice = int(input("Escolha uma opção: "))
+        
+        if choice == 7:
+            print("Saindo do programa.")
+            break
+        
+        if choice == 1 or choice == 2 or choice == 5:
+            matrix = read_matrix(rows, cols)
+        
+        if choice == 1:
+            other_matrix = read_matrix(rows, cols)
+            result = add_matrices(matrix, other_matrix)
+            print_matrix(result)
+        elif choice == 2:
+            other_matrix = read_matrix(rows, cols)
+            result = subtract_matrices(matrix, other_matrix)
+            print_matrix(result)
+        elif choice == 3:
+            scalar = float(input("Digite o valor do escalar: "))
+            result = multiply_matrix_by_scalar(matrix, scalar)
+            print_matrix(result)
+        elif choice == 4:
+            vector = [float(input(f"Digite o valor do elemento {i+1} do vetor: ")) for i in range(rows)]
+            result = multiply_matrix_by_vector(matrix, vector)
+            print(result)
+        elif choice == 5:
+            other_matrix = read_matrix(cols, rows)
+            result = multiply_matrices(matrix, other_matrix)
+            print_matrix(result)
+        elif choice == 6:
+            result = transpose_matrix(matrix)
+            print_matrix(result)
+        else:
+            print("Opção inválida.")
+
+if __name__ == "__main__":
+    main()
